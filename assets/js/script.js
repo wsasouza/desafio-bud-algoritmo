b3a17bfc81c97145e0a482ef29d93bc7f9a77fe0
wsasouza <wsasouza@hotmail.com>
2021-12-15 10:42:35 -0300

    Feat: Implemented up to 6 digits case

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+function findNumberMaxValue(arr) {
+
+  let max = arr.reduce(function(a, b) {
+    return Math.max(a, b);
+  });
+
+  return max;
+
+}
+
+function findNumberMaxIndex(arr) {
+
+  let max = arr[0];
+  let maxIndex = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+          maxIndex = i;
+          max = arr[i];
+      }
+  }
+
+  return maxIndex;
+}
+
+function printMaxNumber() {
+  
+  let data = document.getElementById("data").value;
+  text.innerHTML = '';
+  let arr = data.split("");
+  let size = arr.length;  
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = +arr[i];
+  }
+
+  if (size === 1)     
+    return text.innerHTML = arr;
+  
+
+  else if (size === 2) {
+    result = findNumberMaxValue(arr)
+    return text.innerHTML = result;
+  }
+
+  else if (size <= 6) {
+    let index = findNumberMaxIndex(arr);
+    let value = findNumberMaxValue(arr);
+    let aux = (index + 1) - size;
+
+    if (aux === 0)      
+      return text.innerHTML = value;
+    else 
+      result = arr.slice(index, index+2);
+
+    return text.innerHTML = result.join('');
+  }
+
+  
+
+
+}
